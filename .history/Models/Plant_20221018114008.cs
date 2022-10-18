f80b10505c7a3410785ed0332afb931bba0d304e
Noah <nspielman@live.com>
2022-10-18 12:08:42 -0600

    swagger

---
+++ .history/Models/Plant_20221018114008.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APIv2.models
+{
+    public partial class Plant
+    {
+        public string PlantId { get; set; } = null!;
+        public string? PlantName { get; set; }
+        public string? PlantVariety { get; set; }
+        public string? PlantDate { get; set; }
+        public string GardenId { get; set; } = null!;
+
+        public virtual Garden Garden { get; set; } = null!;
+    }
+}
---
+++ Controllers/TaskController.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APIv2.models;
+
+namespace APIv2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly NyapolaDBContext _context;
+
+        public TaskController(NyapolaDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Task
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<APIv2.models.Task>>> GetTasks()
+        {
+            if (_context.Tasks == null)
+            {
+                return NotFound();
+            }
+            return await _context.Tasks.ToListAsync();
+        }
+
+        // GET: api/Task/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<APIv2.models.Task>> GetTask(string id)
+        {
+            if (_context.Tasks == null)
+            {
+                return NotFound();
+            }
+            var task = await _context.Tasks.FindAsync(id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return task;
+        }
+
+        // PUT: api/Task/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTask(string id, APIv2.models.Task task)
+        {
+            if (id != task.TaskId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(task).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Task
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<APIv2.models.Task>> PostTask(APIv2.models.Task task)
+        {
+            if (_context.Tasks == null)
+            {
+                return Problem("Entity set 'NyapolaDBContext.Tasks'  is null.");
+            }
+            _context.Tasks.Add(task);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TaskExists(task.TaskId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetTask", new { id = task.TaskId }, task);
+        }
+
+        // DELETE: api/Task/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTask(string id)
+        {
+            if (_context.Tasks == null)
+            {
+                return NotFound();
+            }
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TaskExists(string id)
+        {
+            return (_context.Tasks?.Any(e => e.TaskId == id)).GetValueOrDefault();
+        }
+    }
+}
