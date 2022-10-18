a88d31715d935c19d63bd57b7b4bc34534723818
Spielman <nspiel@colostate.edu>
2022-10-17 18:24:18 -0600

    usercontroller

---
+++ .history/Models/Garden_20221016152126.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APIv2
+{
+    public partial class Garden
+    {
+        public Garden()
+        {
+            Calenders = new HashSet<Calender>();
+            Plants = new HashSet<Plant>();
+        }
+
+        public string GardenId { get; set; } = null!;
+        public string? GardenName { get; set; }
+        public string UserId { get; set; } = null!;
+
+        public virtual User User { get; set; } = null!;
+        public virtual ICollection<Calender> Calenders { get; set; }
+        public virtual ICollection<Plant> Plants { get; set; }
+    }
+}
---
+++ .history/Models/User_20221016151814.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APIv2.models
+{
+    public partial class User
+    {
+        public string UserId { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+        public string CustomerName { get; set; } = null!;
+        public string? CustomerEmail { get; set; }
+        public string? CustomerAddress { get; set; }
+
+    
+}
+}
