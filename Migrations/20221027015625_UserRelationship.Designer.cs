cfaaf8550663fd96f27e4347cdd5b9e7bf7ee297
Spielman <nspiel@colostate.edu>
2022-10-26 20:04:30 -0600

    keysAmIRight

--- Migrations/20221027015625_UserRelationship.Designer.cs
+++ Migrations/20221027015625_UserRelationship.Designer.cs
@@ -4,6 +4,7 @@ using APIv2.models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace APIv2.Migrations
 {
     [DbContext(typeof(NyapolaDBContext))]
-    partial class NyapolaDBContextModelSnapshot : ModelSnapshot
+    [Migration("20221027015625_UserRelationship")]
+    partial class UserRelationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
